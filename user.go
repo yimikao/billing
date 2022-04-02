0f37fe8dd0ec7fb7e62e135c61120c6b38ad84e1
yimikao <holayinkajr@gmail.com>
2022-04-02 18:52:47 +0100

    wallet model

--- user.go
+++ user.go
@@ -1,4 +1,4 @@
-package cashapp
+package main
 
 import (
 	"time"
@@ -7,17 +7,17 @@ import (
 )
 
 type Model struct {
+	ID        uuid.UUID `json:"id"`
+	Reference string    `json:"reference"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	DeletedAt time.Time `json:"deleted_at"`
 }
 
 type User struct {
-	ID        uuid.UUID `json:"id"`
-	Reference string    `json:"reference"`
-	Tag       string    `json:"tag"`
-	FullName  string    `json:"full_name"`
-	Email     string    `json:"email"`
+	Tag      string `json:"tag"`
+	FullName string `json:"full_name"`
+	Email    string `json:"email"`
 
 	Model
 }
---
+++ wallet.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/google/uuid"
+
+type Wallet struct {
+	UserID    uuid.UUID `json:"user_id"`
+	IsPrimary bool      `json:"is_primary"`
+	Currency  string    `json:"currency"`
+
+	Model
+}
