4eaf6e72382df27307d9247776d4ae0e2e7e9823
yimikao <holayinkajr@gmail.com>
2022-04-02 19:06:33 +0100

    TransactionEntry Model

---
+++ transaction_entries.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/google/uuid"
+
+type TransactionType string
+
+const (
+	Credit TransactionType = "credit"
+	Debit  TransactionType = "debit"
+)
+
+type TransactionEntry struct {
+	Model
+
+	TransactionID uuid.UUID       `json:"transaction_id"`
+	WalletID      uuid.UUID       `json:"wallet_id"`
+	Type          TransactionType `json:"type"`
+	Amount        int64           `json:"amount"`
+}
