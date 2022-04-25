e4debdb4a6fd12e0f184f3e303d6a6ab376b9e2b
yimikao <holayinkajr@gmail.com>
2022-04-25 16:55:58 +0100

    update wallettransaction

--- transaction.go
+++ transaction.go
@@ -2,31 +2,54 @@ package billing
 
 import "github.com/google/uuid"
 
-type TransactionDirection string
-type TransactionStatus string
-type TransactionPurpose string
+// type TransactionDirection string
+// type TransactionStatus string
+// type TransactionPurpose string
+
+type TransactionDirection uint8
+type TransactionStatus uint8
+type TransactionPurpose uint8
 
 const (
-	Incoming TransactionDirection = "incoming"
-	Outgoing TransactionDirection = "outgoing"
-
-	Failed  TransactionStatus = "failed"
-	Pending TransactionStatus = "pending"
-	Success TransactionStatus = "success"
-
-	Transfer   TransactionPurpose = "transfer"
-	Deposit    TransactionPurpose = "deposit"
-	Withdrawal TransactionPurpose = "withdrawal"
-	Reversal   TransactionPurpose = "reversal"
+	PendingTransactionStatus TransactionStatus = iota
+	SuccessfulTransactionStatus
+	FailedTransactionStatus
 )
 
-type Transaction struct {
+const (
+	IncomingTransactionDirection TransactionDirection = iota
+	OutgoingTransactionDirection
+)
+
+const (
+	TransferTransactionPurpose TransactionPurpose = iota
+	DepositTransactionPurpose
+	WithdrawalTransactionPurpose
+	ReversalTransactionPurpose
+)
+
+// const (
+// 	Incoming TransactionDirection = "incoming"
+// 	Outgoing TransactionDirection = "outgoing"
+
+// 	Failed  TransactionStatus = "failed"
+// 	Pending TransactionStatus = "pending"
+// 	Success TransactionStatus = "success"
+
+// 	Transfer   TransactionPurpose = "transfer"
+// 	Deposit    TransactionPurpose = "deposit"
+// 	Withdrawal TransactionPurpose = "withdrawal"
+// 	Reversal   TransactionPurpose = "reversal"
+// )
+
+type WalletTransaction struct {
 	Model
 
 	From     uuid.UUID `json:"from"`
 	To       uuid.UUID `json:"to"`
 	WalletID uuid.UUID `json:"wallet_id"`
 	Amount   int64     `json:"amount"`
+	Currency string    `json:"currency"`
 
 	Status        TransactionStatus    `json:"status"`
 	FailureReason string               `json:"failure_reason"`
