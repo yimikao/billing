632ab2b584e673bdc29071ac89e504f4dc8d5e02
yimikao <holayinkajr@gmail.com>
2022-04-27 21:49:03 +0100

    update db layer

---
+++ database/postgres/postgres.go
@@ -0,0 +1,30 @@
+package postgres
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-pg/pg/v10"
+)
+
+func New(dsn string) (*pg.DB, error) {
+
+	opts, err := pg.ParseURL(dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	db := pg.Connect(opts)
+
+	if err := db.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
+
+func WithContext() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), time.Second*4)
+}
