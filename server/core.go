ef02584d2285cd10335b4bdcc7c0068f293761de
yimikao <holayinkajr@gmail.com>
2022-04-28 11:42:56 +0100

    create core/auth handlers

--- server/core.go
+++ server/core.go
@@ -1,122 +1,116 @@
 package server
 
 import (
-	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
-	"net/url"
-	"strings"
+	"time"
 
-	"github.com/fluidcoins/log"
-	"github.com/go-chi/render"
-	"github.com/yimikao/billing/core"
+	applogger "github.com/fluidcoins/log"
+	"github.com/yimikao/billing/core/oauth"
 	"golang.org/x/oauth2"
 )
 
-type LoginHandler struct {
-	OauthConfig *oauth2.Config
-	OauthState  string
-	logger      log.Entry
-}
+func generateStateOauthCookie(w http.ResponseWriter) string {
+	var expiration = time.Now().Add(365 * 24 * time.Hour)
 
-func NewLoginHandler() *LoginHandler {
-	return &LoginHandler{
-		OauthConfig: core.OauthConfig,
-		OauthState:  core.OauthState,
+	b := make([]byte, 16)
+	rand.Read(b)
+
+	state := base64.URLEncoding.EncodeToString(b)
+
+	cookie := http.Cookie{
+		Name:    "oauthstate",
+		Value:   state,
+		Expires: expiration,
 	}
+
+	http.SetCookie(w, &cookie)
+
+	// return cookie value
+	return state
 }
 
-func NewCallbackHandler() *CallbackHandler {
-	return &CallbackHandler{
-		OauthConfig: core.OauthConfig,
-		OauthState:  core.OauthState,
+type LoginHandler struct {
+	cfg    *oauth2.Config
+	logger applogger.Entry
+}
+
+func NewLoginHandler(cfg *oauth2.Config, logger applogger.Entry) *LoginHandler {
+	return &LoginHandler{
+		cfg:    cfg,
+		logger: logger,
 	}
 }
 
 func (h *LoginHandler) Login(w http.ResponseWriter, r *http.Request) {
 
-	URL, err := url.Parse(h.OauthConfig.Endpoint.AuthURL)
+	// add cookie to response and also return it's value
+	// 'll be validated that it matches with the state query parameter on redirect callback
+	oauthState := generateStateOauthCookie(w)
 
-	if err != nil {
-		h.logger.WithError(err).Error(err.Error())
-		_ = render.Render(w, r, newAPIError(http.StatusBadRequest, "invalid request body"))
-		return
-	}
-
-	params := url.Values{}
-	params.Add("client_id", h.OauthConfig.ClientID)
-	params.Add("scope", strings.Join(h.OauthConfig.Scopes, " "))
-	params.Add("redirect_uri", h.OauthConfig.RedirectURL)
-	params.Add("response_type", "code")
-	params.Add("state", h.OauthState)
-
-	URL.RawQuery = params.Encode()
-
-	url := URL.String()
-
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	// redirect user to googleauth url
+	u := h.cfg.AuthCodeURL(oauthState)
+	http.Redirect(w, r, u, http.StatusTemporaryRedirect)
 
 }
 
 type CallbackHandler struct {
-	OauthConfig *oauth2.Config
-	OauthState  string
-	Logger      log.Entry
+	client *oauth.GoogleOauthClient
+	logger applogger.Entry
+}
+
+func NewCallbackHandler(client *oauth.GoogleOauthClient, logger applogger.Entry) *CallbackHandler {
+	return &CallbackHandler{
+		client: client,
+		logger: logger,
+	}
 }
 
 func (h *CallbackHandler) Callback(w http.ResponseWriter, r *http.Request) {
 
-	state := r.FormValue("state")
-	if state != h.OauthState {
-		// h.logger.Error("invalid oauth state")
-		// _ = render.Render(w, r, newAPIError(http.StatusBadRequest, "invalid oauth state"))
-		fmt.Println("invalid oauth state")
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	oauthStateCookie, _ := r.Cookie("oauthstate")
+
+	if r.FormValue("state") != oauthStateCookie.Value {
+		log.Println("invalid oauth google state")
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
 
 	code := r.FormValue("code")
 	if code == "" {
-
-		// _ = render.Render(w, r, newAPIError(http.StatusBadRequest, "code not found to provide token"))
-		fmt.Println("code to provide token not found")
+		log.Println("auth code not supplied")
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-
-		reason := r.FormValue("error_reason")
-		if reason == "user_denied" {
-			// _ = render.Render(w, r, newAPIError(http.StatusBadRequest, "user has denied perm"))
-			fmt.Println("user has denied perm")
-			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		}
-
-	} else {
-		token, err := h.OauthConfig.Exchange(context.Background(), code)
-		if err != nil {
-			fmt.Println("oauth exchange failed" + err.Error())
-			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-			return
-		}
-
-		resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + url.QueryEscape(token.AccessToken))
-		if err != nil {
-			// logger.Log.Error("Get: " + err.Error() + "\n")
-			fmt.Println("getting body error" + err.Error())
-			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-			return
-		}
-		defer resp.Body.Close()
-
-		response, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("reading body err" + err.Error())
-			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-			return
-		}
-
-		fmt.Println(response)
-		_ = render.Render(w, r, newAPIStatus(http.StatusOK, "login successful"))
-
+		return
 	}
 
+	data, err := h.client.GetUserDataFromGoogle(code)
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	var ur = new(UserResponse)
+	if err := json.Unmarshal(data, ur); err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprintf(w, "UserInfo: %s\n %s\n", ur.Email, ur.Picture)
+
+	// t, _ := template.ParseFiles("templates/success.html")
+	// t.Execute(w, ur)
+	// fmt.Fprintf(w, "UserInfo: %s\n", data)
 }
